4632261d7148e1297a798b5d4b6e35012c59d7ba
1ammer <ammeruol@gmail.com>
2019-02-22 21:46:26 +0500

    del

--- resources/views/quiz/layouts/success.blade.php
+++
@@ -1,8 +0,0 @@
-@if (\Session::has('success'))
-    <div class="alert alert-success fade in alert-dismissible" style="opacity: 5;">
-        <a href="#" class="close pull-right" data-dismiss="alert">×</a>
-        <i class="fa fa-check-circle"></i> <ul>
-            <li>{!! \Session::get('success') !!}</li>
-        </ul>
-    </div>
-@endif
